455306aa08538f0910fa86fada87d8204815bc0d
[ajay-r-dev] <[ajayrcloudengineer@gmail.com]>
2024-08-03 16:05:22 +0530

    added body

---
+++ src/app/body-section/body-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './body-section.component.html',
+  styleUrl: './body-section.component.css'
+})
+export class BodySectionComponent {
+
+}
